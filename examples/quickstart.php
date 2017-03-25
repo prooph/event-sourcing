379389e05e09c4d46b12536af2cca39273df22df
prolic <saschaprolic@googlemail.com>
2017-03-25 15:47:30 +0800

    update quickstart

--- examples/quickstart.php
+++ examples/quickstart.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * This file is part of the prooph/event-sourcing.
- * (c) 2014-2016 prooph software GmbH <contact@prooph.de>
- * (c) 2015-2016 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ * (c) 2014-2017 prooph software GmbH <contact@prooph.de>
+ * (c) 2015-2017 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -62,9 +62,6 @@ namespace My\Model {
             return $this->name;
         }
 
-        /**
-         * @param $newName
-         */
         public function changeName(string $newName): void
         {
             Assertion::notEmpty($newName);
@@ -78,38 +75,30 @@ namespace My\Model {
         }
 
         /**
-         * Each applied event needs a corresponding handler method.
-         *
-         * The naming convention is: when[:ShortEventName]
-         */
-        protected function whenUserWasCreated(UserWasCreated $event): void
-        {
-            //Simply assign the event payload to the appropriate properties
-            $this->uuid = Uuid::fromString($event->aggregateId());
-            $this->name = $event->username();
-        }
-
-        protected function whenUserWasRenamed(UserWasRenamed $event): void
-        {
-            $this->name = $event->newName();
-        }
-
-        /**
          * Every AR needs a hidden method that returns the identifier of the AR as a string
          */
         protected function aggregateId(): string
         {
             return $this->uuid->toString();
         }
+
+        protected function apply(AggregateChanged $event): void
+        {
+            switch (get_class($event)) {
+                case UserWasCreated::class:
+                    //Simply assign the event payload to the appropriate properties
+                    $this->uuid = Uuid::fromString($event->aggregateId());
+                    $this->name = $event->username();
+                    break;
+                case UserWasRenamed::class:
+                    $this->name = $event->newName();
+                    break;
+            }
+        }
     }
 
     /**
-     * Class UserWasCreated
-     *
      * ProophEventSourcing domain events are of the type AggregateChanged
-     *
-     * @package My\Model
-     * @author Alexander Miertsch <kontakt@codeliner.ws>
      */
     class UserWasCreated extends AggregateChanged
     {
@@ -120,12 +109,7 @@ namespace My\Model {
     }
 
     /**
-     * Class UserWasRenamed
-     *
      * ProophEventSourcing domain events are of the type AggregateChanged
-     *
-     * @package My\Model
-     * @author Alexander Miertsch <kontakt@codeliner.ws>
      */
     class UserWasRenamed extends AggregateChanged
     {
@@ -141,18 +125,13 @@ namespace My\Model {
     }
 
     /**
-     * Interface UserRepository
-     *
      * Simple interface for a user repository
-     *
-     * @package My\Model
-     * @author Alexander Miertsch <kontakt@codeliner.ws>
      */
     interface UserRepository
     {
-        public function add(User $user): void;
+        public function save(User $user): void;
 
-        public function get(Uuid $uuid): User;
+        public function get(Uuid $uuid): ?User;
     }
 }
 
@@ -165,12 +144,6 @@ namespace My\Infrastructure {
     use Prooph\EventStore\EventStore;
     use Ramsey\Uuid\Uuid;
 
-    /**
-     * Class UserRepositoryImpl extends Prooph\EventSourcing\Aggregate\AggregateRepository and implements My\Model\UserRepository
-     *
-     * @package My\Infrastructure
-     * @author Alexander Miertsch <kontakt@codeliner.ws>
-     */
     class UserRepositoryImpl extends AggregateRepository implements UserRepository
     {
         public function __construct(EventStore $eventStore)
@@ -186,15 +159,12 @@ namespace My\Infrastructure {
             );
         }
 
-        /**
-         * @param User $user
-         */
-        public function add(User $user): void
+        public function save(User $user): void
         {
-            $this->addAggregateRoot($user);
+            $this->saveAggregateRoot($user);
         }
 
-        public function get(Uuid $uuid): User
+        public function get(Uuid $uuid): ?User
         {
             return $this->getAggregateRoot($uuid->toString());
         }
@@ -202,14 +172,18 @@ namespace My\Infrastructure {
 }
 
 namespace {
-    //Set up an EventStore with an InMemoryAdapter (Only useful for testing, persistent adapters for ProophEventStore are available)
+    //Set up an EventStore with an InMemoryAdapter (Only useful for testing, persistent implementations of ProophEventStore are available)
     use My\Infrastructure\UserRepositoryImpl;
     use My\Model\User;
     use Prooph\Common\Event\ActionEvent;
     use Prooph\Common\Event\ProophActionEventEmitter;
     use Prooph\EventStore\InMemoryEventStore;
+    use Prooph\EventStore\TransactionalActionEventEmitterEventStore;
 
-    $eventStore = new InMemoryEventStore(new ProophActionEventEmitter());
+    $eventStore = new TransactionalActionEventEmitterEventStore(
+        new InMemoryEventStore(),
+            new ProophActionEventEmitter()
+    );
 
     //Now we set up our user repository and inject the EventStore
     //Normally this should be done in an IoC-Container and the receiver of the repository should require My\Model\UserRepository
@@ -220,18 +194,31 @@ namespace {
 
     $user = User::nameNew('John Doe');
 
-    $userRepository->add($user);
+    //Before we save let's attach a listener to check that the UserWasCreated event is recorded
+    $eventStore->attach(
+        TransactionalActionEventEmitterEventStore::EVENT_CREATE,
+        function (ActionEvent $event): void {
+            foreach ($event->getParam('stream')->streamEvents() as $streamEvent) {
+                echo sprintf(
+                    'Event with name %s was recorded. It occurred on %s UTC /// ',
+                    $streamEvent->messageName(),
+                    $streamEvent->createdAt()->format('Y-m-d H:i:s')
+                ) . PHP_EOL;
+            }
+        },
+        -1000
+    );
 
-    //Before we commit the transaction let's attach a listener to check that the UserWasCreated event is published after commit
-    $eventStore->getActionEventEmitter()->attachListener('commit.post', function (ActionEvent $event): void {
-        foreach ($event->getParam('recordedEvents', new \ArrayIterator()) as $streamEvent) {
-            echo sprintf(
-                'Event with name %s was recorded. It occurred on %s UTC /// ',
-                $streamEvent->messageName(),
-                $streamEvent->createdAt()->format('Y-m-d H:i:s')
-            );
-        }
-    });
+    $userRepository->save($user);
+
+    //Let's make sure the transaction is written
+    $eventStore->attach(
+        TransactionalActionEventEmitterEventStore::EVENT_COMMIT,
+        function (ActionEvent $event): void {
+            echo 'Transaction commited' . PHP_EOL;
+        },
+        -1000
+    );
 
     $eventStore->commit();
 
@@ -248,6 +235,23 @@ namespace {
     $loadedUser = $userRepository->get($userId);
 
     $loadedUser->changeName('Max Mustermann');
+
+    //Before we save let's attach a listener again on appendTo to check that the UserWasRenamed event is recorded
+    $eventStore->attach(
+        TransactionalActionEventEmitterEventStore::EVENT_APPEND_TO,
+        function (ActionEvent $event): void {
+            foreach ($event->getParam('streamEvents') as $streamEvent) {
+                echo sprintf(
+                        'Event with name %s was recorded. It occurred on %s UTC /// ',
+                        $streamEvent->messageName(),
+                        $streamEvent->createdAt()->format('Y-m-d H:i:s')
+                    ) . PHP_EOL;
+            }
+        },
+        -1000
+    );
+
+    $userRepository->save($loadedUser);
 
     //... so we only need to commit the transaction and the UserWasRenamed event should be recorded
     //(check output of the previously attached listener)
